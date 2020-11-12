3bbe8e8b05fba9fbcd695ab0736e689c1e20632e
yash <yashagrawal9265@gmail.com>
2020-11-12 19:40:05 +0530

    set a post request in a index.html

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask, redirect, url_for, render_template, request, flash
 
 app = Flask(__name__)
-@app.route('/')
+@app.route('/', methods=["GET","POST"])
 def home():
+    if request.method == "POST":
+        passLength = request.form.get('passLength')
+        passSymbols = request.form.get('passSymbols')
+        passNumbers = request.form.get('passNumbers')
+        passLower = request.form.get('passLower')
+        passUpper = request.form.get('passUpper')
+        print(f"{passLength}, {passSymbols}, {passNumbers}, {passLower}, {passUpper}");
+        return redirect(url_for('home'))
+
     return render_template('index.html')
 
 
