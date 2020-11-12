df5ca5c12c00247b71a1c9f46cc3d70fc0bc1841
yash <yashagrawal9265@gmail.com>
2020-11-13 01:44:48 +0530

    Now user can send a post request and can generate a new password

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from flask import Flask, redirect, url_for, render_template, request, flash
+import string
+import random
+
+lowerCaseCharacter = string.ascii_lowercase
+upperCaseCharacter = string.ascii_uppercase
+numericDigits = string.digits
+specialSymbols= string.punctuation
 
 app = Flask(__name__)
 @app.route('/', methods=["GET","POST"])
@@ -9,8 +16,23 @@ def home():
         passNumbers = request.form.get('passNumbers')
         passLower = request.form.get('passLower')
         passUpper = request.form.get('passUpper')
-        print(f"{passLength}, {passSymbols}, {passNumbers}, {passLower}, {passUpper}");
-        return redirect(url_for('home'))
+
+        # Algorithm for generation password
+        password = []
+        if passSymbols == 'on':
+            password.extend(specialSymbols)
+            password.extend(lowerCaseCharacter)
+            password.extend(upperCaseCharacter)
+            password.extend(numericDigits)
+        else:   
+            password.extend(lowerCaseCharacter)
+            password.extend(upperCaseCharacter)
+            password.extend(numericDigits)
+
+        password=random.sample(password,int(passLength))
+        newPassword = "".join(password)
+        # print(f"{passLength}, {passSymbols}, {passNumbers}, {passLower}, {passUpper}");
+        return render_template('index.html', newPassword= newPassword)
 
     return render_template('index.html')
 
